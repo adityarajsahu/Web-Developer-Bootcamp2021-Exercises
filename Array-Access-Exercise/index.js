c7ecd7c2274218ac985408a748e51fc3126f3e83
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2022-06-10 11:14:45 +0530

    access array

---
+++ Array-Access-Exercise/index.js
@@ -0,0 +1,6 @@
+const leaderboard = ['Harry', 'Lua', 'Hermione', 'Bellatrix']; //DON'T TOUCH THIS LINE!
+
+// YOUR CODE BELOW HERE:
+leaderboard[1] = 'Luna';
+leaderboard.pop();
+leaderboard.push('Draco');
